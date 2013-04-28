c3e4154748013e1143a44cdea37832b27286d363
yankai0219 <yankai0219@126.com>
2013-04-28 20:20:22 +0800

    Update log.h

--- log.h
+++ log.h
@@ -1,24 +1,26 @@
+
+/*
+* Copyright (C) Yankai
+*/
+
+
 #ifndef _LOG_H_
 #define _LOG_H_
-#include <string.h>
-#include <time.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdarg.h>
-#define _MYDEBUG_ 1
-#if _MYDEBUG_
-   #define DEBUG(fmt,...) printf("YK %s:%d "fmt"",__FILE__,__LINE__,##__VA_ARGS__)
-#else
-   #define DEBUG(fmt,...)
-#endif
+
+#include "common.h"
+
+
 typedef struct log_s log_t;
 typedef int (*write_log_pt)(log_t *log,char *fmt,...);
 typedef int (*open_log_pt)(log_t *log,char *filepath);
 typedef int (*free_log_pt)(log_t *log);
 
+
 int free_log(log_t *log);
 int open_log(log_t *log,char *filepath);
 int write_log(log_t *log,char *fmt,...);
+
+
 struct log_s {
     FILE* fp;
     char *file_path;
@@ -26,8 +28,9 @@ struct log_s {
     open_log_pt openlog;
     free_log_pt freelog;
 };
+
+
 void err_exit(char *fmt,...);
 
 
 #endif
-
